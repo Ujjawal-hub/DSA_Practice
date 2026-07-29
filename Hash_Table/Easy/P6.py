afeddb92d002eb0c853d0470e626dd229c5a3890
Ujjawal-hub <jainujjawal22@gmail.com>
2026-07-29 21:52:49 +0530

    Leet_Code

--- Hash_Table/Easy/P6.py
+++ Hash_Table/Easy/P6.py
@@ -35,3 +35,41 @@ class Solution:
         return next(iter(a))
 
 #This is BigO(n) in space and time
+
+# Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        a = 0
+        b = 0
+
+        while b < len(nums):
+
+            if nums[a] == nums[b]:
+
+                b += 1
+
+            elif nums[a] == None:
+
+                a += 1
+
+            else:
+
+                nums[a] = None
+                nums[b] = None
+
+                a += 1
+                b += 1
+
+        i = 0
+
+        while nums[i] is None:
+            i += 1
+
+        return nums[i]
+
+# This Is BigO(N) in Time and BigO(1) in space
+
+     # there was lot of confusion there ,if there is a edge case, where,minority might survive but those assumtion wrong, becasue that can only happen if there some differnt value exist between a and b,but that is not possible in between a and b always contain same number
+        #which got skip due to duplication
