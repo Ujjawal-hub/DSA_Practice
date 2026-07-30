090793b2ac89574472b0025895c80bb5e178768d
Ujjawal-hub <jainujjawal22@gmail.com>
2026-07-30 21:33:52 +0530

    Leet_Code

---
+++ Hash_Table/Medium/P1.py
@@ -0,0 +1,174 @@
+# Given an array of strings strs, group the anagrams together.
+# You can return the answer in any order.
+
+class Solution:
+
+    def isAnagram(self, string1, string2):
+
+        anagramdict = dict()
+
+        if len(string1) != len(string2):
+            return False
+
+        for a in string1:
+
+            if a in anagramdict:
+
+                anagramdict[a] += 1
+            else:
+
+                anagramdict[a] = 1
+
+        for b in string2:
+
+            if b in anagramdict:
+
+                anagramdict[b] -= 1
+
+                if anagramdict[b] == 0:
+                    anagramdict.pop(b)
+            else:
+
+                return False
+
+        return True
+
+    def groupAnagram_Jr(self, strs):
+
+        a = list()
+        b = list()
+        b.append(strs[0])
+        a.append(b)
+
+        i = 1
+
+        while i < len(strs):
+
+            j = 0
+
+            while j < len(a):
+
+                if self.isAnagram(a[j][0], strs[i]):
+                    a[j].append(strs[i])
+
+                    break
+
+                j += 1
+
+            else:
+
+                b = [strs[i]]
+
+                a.append(b)
+
+            i += 1
+
+        return a
+
+    def groupAnagrams(self, strs) :
+
+        lendict = dict()
+
+        answer = list()
+
+        for a in strs:
+
+            if len(a) in lendict:
+
+                # lendict[len(a)] = lendict[len(a)].append(a)  wrong key is just a pointer to list apeend will retun none so pointer will point to none
+
+                lendict[len(a)].append(a)
+
+            else:
+
+                lendict[len(a)] = [a]
+
+        for b in lendict:
+
+            array = self.groupAnagram_Jr(lendict[b])
+
+            for i in array:
+                answer.append(i)
+
+        return answer
+
+ #this is BigO((N^2)*k) in Time and BigO(N) in space (if we traet a string as a single unit of space) otherwise it is BigO(Nk),where N is number of string in the l=array and k is number of character in the string
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        groupdict = dict()
+
+        for string in strs:
+
+            anagram = dict()
+
+            for character in string:
+
+                if character in anagram:
+
+                    anagram[character] += 1
+                else:
+
+                    anagram[character] = 1
+
+            key = str(anagram)
+
+            if key in groupdict:
+
+                groupdict[key].append(string)
+
+            else:
+
+                groupdict[key] = [string]
+
+        answer = list()
+
+        for a in groupdict:
+            answer.append(groupdict[a])
+
+        return answer
+
+        # for a in groupdict:
+
+        #     answer.append(groupdict.pop(a))
+
+        # return answer
+
+        # iterator = iter(groupdict)
+
+        # while len(groupdict) != 0:
+
+        #     b = next(iterator)
+
+        #     c = groupdict.pop(b)
+
+        #     answer.append(c)
+
+        # return answer
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        groupdict = dict()
+
+        for string in strs:
+
+            key = "".join(sorted(string))
+
+            if key in groupdict:
+
+                groupdict[key].append(string)
+
+            else:
+
+                groupdict[key] = [string]
+
+        answer = list()
+
+        for a in groupdict:
+
+            answer.append(groupdict[a])
+
+        return answer
