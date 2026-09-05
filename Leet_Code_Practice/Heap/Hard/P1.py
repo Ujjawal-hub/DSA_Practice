e27a7e4c2047c3d8eda6c0148d59689baa36a88c
Ujjawal-hub <jainujjawal22@gmail.com>
2026-09-05 14:56:59 +0530

    Leet_Code

--- Leet_Code_Practice/Heap/Hard/P1.py
+++ Leet_Code_Practice/Heap/Hard/P1.py
@@ -40,3 +40,92 @@ class MedianFinder:
 
 
     #this BigO(logn) in Time and BigO(n) in space
+
+
+    #Follow up:
+
+# If all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+# If 99% of all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+    #below two are standerd solutions for the follow up
+
+class BoundedMedianFinder:
+
+    def __init__(self):
+        self.counts = [0] * 101  # Direct indexing for values 0 through 100
+        self.total_count = 0
+
+    def addNum(self, num: int) -> None:
+          self.counts[num] += 1
+          self.total_count += 1
+
+    def findMedian(self) -> float:          # Target 1-based ranks for the middle element(s)
+        target1 = (self.total_count + 1) // 2
+        target2 = (self.total_count // 2) + 1
+
+        count_so_far = 0
+        val1, val2 = None, None
+
+            # Linear scan across at most 101 buckets: O(1) time
+        for val in range(101):
+            count_so_far += self.counts[val]
+
+            if val1 is None and count_so_far >= target1:
+                val1 = val
+            if val2 is None and count_so_far >= target2:
+                val2 = val
+                break
+
+        if self.total_count % 2 != 0:
+            return float(val1)
+        return (val1 + val2) / 2.0
+
+
+class SparseBoundedMedianFinder:
+
+  def __init__(self):
+    self.counts = [0] * 101  # Frequency buckets for 99% of values
+    self.below = []  # Max-heap for outliers < 0 (negated values)
+    self.above = []  # Min-heap for outliers > 100
+    self.total_count = 0
+
+  def addNum(self, num: int) -> None:
+    if num < 0:
+      heapq.heappush(self.below, -num)
+    elif num > 100:
+      heapq.heappush(self.above, num)
+    else:
+      self.counts[num] += 1
+    self.total_count += 1
+
+  def findMedian(self) -> float:
+    target1 = (self.total_count + 1) // 2
+    target2 = (self.total_count // 2) + 1
+
+    def get_val_at_rank(rank: int) -> int:
+      # 1. Rank falls in 'below' heap (< 0)
+      if rank <= len(self.below):
+        # Extract k-th element from max-heap without mutation
+        sorted_below = sorted([-x for x in self.below])
+        return sorted_below[rank - 1]
+
+      # 2. Rank falls in [0, 100] bucket range
+      current_rank = len(self.below)
+      for val in range(101):
+        current_rank += self.counts[val]
+        if current_rank >= rank:
+          return val
+
+      # 3. Rank falls in 'above' heap (> 100)
+      above_rank = rank - current_rank
+      sorted_above = sorted(self.above)
+      return sorted_above[above_rank - 1]
+
+    val1 = get_val_at_rank(target1)
+    if self.total_count % 2 != 0:
+      return float(val1)
+
+    val2 = get_val_at_rank(target2)
+    return (val1 + val2) / 2.0
+
+
+
